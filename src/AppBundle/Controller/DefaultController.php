2758cebbba58ec6f527340033249738ea6ba5f2a
Pepe Doval <pepellou@gmail.com>
2017-09-13 11:13:56 +0200

    Sample controller to create/show user

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
 
 class DefaultController extends Controller
 {
@@ -18,4 +19,37 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    public function createUserAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setEmail('pepellou@gmail.com');
+        $user->setPassword('aPassword');
+        $user->setCompanyName('Agil AZ S.L.');
+        $user->setCif('B70240296');
+        $user->setAddress('c/ Hedras, 6, 1D');
+        $user->setContactPerson('Pepe Doval');
+
+        $em->persist($user);
+
+        $em->flush();
+
+        return new Response('Created new user with id '.$user->getId());
+    }
+
+    public function showUserAction($userId) {
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->find($userId);
+
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No user found for id '.$userId
+            );
+        }
+
+        return new Response('Found user with id '.$user->getId().' and email '.$user->getEmail());
+    }
+
 }

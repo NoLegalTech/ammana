47643db23c241083a76ad33544fd0c017167ad9d
Pepe Doval <pepellou@gmail.com>
2017-09-15 14:02:28 +0200

    Fill profile with data from DB

--- src/ProfileBundle/Controller/DefaultController.php
+++ src/ProfileBundle/Controller/DefaultController.php
@@ -2,12 +2,30 @@
 
 namespace ProfileBundle\Controller;
 
+use Psr\Log\LoggerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+
+use AppBundle\Entity\User;
 
 class DefaultController extends Controller
 {
     public function indexAction() {
-        return $this->render('user/profile.html.twig');
+        $id = 1; // TODO get from session when there's login
+
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No user found for id '.$id
+            );
+        }
+
+        return $this->render('user/profile.html.twig', array(
+            'user' => $user,
+        ));
     }
 }
--- tests/ProfileBundle/Controller/DefaultControllerTest.php
+++ tests/ProfileBundle/Controller/DefaultControllerTest.php
@@ -12,6 +12,13 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/profile');
 
+        //$content = $client->getResponse()->getContent();
+
         $this->assertContains('Tu perfil', $client->getResponse()->getContent());
+        $this->assertContains('Nombre de la empresa:', $client->getResponse()->getContent());
+        $this->assertContains('CIF:', $client->getResponse()->getContent());
+        $this->assertContains('Domicilio social:', $client->getResponse()->getContent());
+        $this->assertContains('Persona de contacto:', $client->getResponse()->getContent());
+        $this->assertContains('Sector:', $client->getResponse()->getContent());
     }
 }

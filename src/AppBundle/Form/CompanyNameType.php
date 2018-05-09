6700d250d8bc89a9f859301af6b05d9a4be301a6
Pepe Doval <pepellou@gmail.com>
2018-05-09 20:49:15 +0200

    Admin can create protocols

---
+++ src/AppBundle/Form/CompanyNameType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CompanyNameType extends AbstractType {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $i18n = $options['i18n'];
+        $builder
+            ->add('companyName', TextType::class, array(
+                'label' => $i18n['forms']['company_name_form']['company_name'] . ':',
+                'required' => true
+            ))
+            ->add('logo', FileType::class, array(
+                'label' => $i18n['forms']['company_name_form']['logo'] . ':',
+                'required' => false
+            ));
+        $builder->get('logo')
+            ->addModelTransformer(new CallBackTransformer(
+                function($imageUrl) {
+                    return null;
+                },
+                function($imageUrl) {
+                    return $imageUrl;
+                }
+            ));
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired('i18n');
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Company'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'company_name_form';
+    }
+
+}

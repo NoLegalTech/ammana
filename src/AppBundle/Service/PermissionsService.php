b6c7b51516d05f29e247a31adb98d1b21f4330f2
Pepe Doval <pepellou@gmail.com>
2018-07-26 14:10:36 +0200

    Add copyright notice to all sources

--- src/AppBundle/Service/PermissionsService.php
+++ src/AppBundle/Service/PermissionsService.php
@@ -1,5 +1,12 @@
 <?php
 
+/*!
+ * ammana.es - job protocols generator
+ * https://github.com/NoLegalTech/ammana
+ * Copyright (C) 2018 Zeres Abogados y Consultores Laborales SLP <zeres@zeres.es>
+ * https://github.com/NoLegalTech/ammana/blob/master/LICENSE
+ */
+
 namespace AppBundle\Service;
 
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
